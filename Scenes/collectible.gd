f0fae8ed3dffda111ad39e2ff868d84e137c630c
Kafstinolli <agomez47@uan.edu.co>
2025-10-31 12:08:48 -0500

    Implementado 'AudioManager' como singleton para manejar música y efectos. Eliminados audios duplicados de los niveles y actualizadas referencias en 'coleccionable.tscn' y 'menu.gd'.

--- Scenes/collectible.gd
+++ Scenes/collectible.gd
@@ -1,12 +1,15 @@
 # Este script se usa para detectar cuando el personaje toca un área (por ejemplo, una moneda o ítem).
 extends Area2D
 
-# Referencia al texto donde se muestran los puntos (aunque aquí no se usa directamente).
-@onready var points_label: Label = %PointsLabel
+var coin_sound
+
+func _ready():
+	coin_sound = get_node("/root/Node/CoinSound")
 
 # Esta función se ejecuta cuando otro cuerpo (body) entra en el área.
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
 		# Si el objeto que entra es el jugador:
 		SingleGameManager.add_puntos() # Llama al método que suma puntos.
+		coin_sound.playing = true
 		queue_free()  # Elimina este objeto de la escena (la cereza desaparece).
--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -2,13 +2,19 @@
 extends Node
 
 var ui_node
+var musica_fondo
 
 func _ready():
 	ui_node = get_node("/root/Ui")
 	ui_node.hide()
+	
+	# Referencia a la música de fondo en la escena UI
+	musica_fondo = get_node("/root/Ui/GameAudio")
+	musica_fondo.stop()  # Detiene la música mientras se está en el menú
 
 func _exit_tree():
 	ui_node.show()
+	musica_fondo.play()  # Reanuda la música al salir del menú
 
 func _on_nivel_1_pressed() -> void:
 	# Se ejecuta cuando el botón "Nivel 1" es presionado.
